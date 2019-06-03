9a944d333588c156dc19442add891853416a48ee
Victor Recabarren <victormrecabarren@gmail.com>
2019-06-02 22:21:08 -0500

    able to set lineups to checked or unchecked and save to db, makes two db calls..

--- controllers/saturdayitinerary.js
+++ controllers/saturdayitinerary.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const SaturdayItinerary = require('../models/itinerary');
+const SaturdayLineup = require('../models/artists')
 
 router.get('/', (req, res) => {
   res.send('itinerary page')
 });
 
 router.post('/', (req, res) => {
-  res.send(req.body)
+    checkedArray = [];
+    uncheckedArray = [];
+    for (keys in req.body) {
+      if (req.body[keys] == 0) {
+        req.body[keys] = false;
+        uncheckedArray.push(keys)
+      }
+      else {
+        req.body[keys] = true;
+      checkedArray.push(keys);
+    }
+  }
+
+    SaturdayLineup.updateMany({_id: {$in: uncheckedArray}}, {$set: {checked: false}}, {new: true}, (err, data) => {
+      console.log('these weree unchecked:', data);
+    });
+
+    SaturdayLineup.updateMany({_id: {$in: checkedArray}}, {$set: {checked: true}}, {new: true}, (err, data) => {
+      console.log('these weree checked:', data);
+    });
+
+    // console.log('checked array', checkedArray);
+    // console.log('unchecked array', uncheckedArray);
+
+
+    res.send(req.body)
+
 })
 
 

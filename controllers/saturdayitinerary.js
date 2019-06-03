4f7e7937a05e18573984911d81e36a83177094ea
Victor Recabarren <victormrecabarren@gmail.com>
2019-06-02 23:23:43 -0500

    able to update lineup and create new user itinerary based on form in index page

--- controllers/saturdayitinerary.js
+++ controllers/saturdayitinerary.js
@@ -1,38 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const SaturdayItinerary = require('../models/itinerary');
-const SaturdayLineup = require('../models/artists')
+const SaturdayLineup = require('../models/artists');
+
+
+
+// functions
+const addItineraryItem = (info) => {
+  checkedArray = [];
+  uncheckedArray = [];
+  for (keys in info) {
+    if (info[keys] == 0) {
+      info[keys] = false;
+      uncheckedArray.push(keys)
+    }
+    else {
+      info[keys] = true;
+    checkedArray.push(keys);
+    }
+  }
+
+  SaturdayLineup.updateMany({_id: {$in: uncheckedArray}}, {$set: {checked: false}}, {multi: true}, (err, unchecked) => {
+    if (err) console.log(err);
+  });
+
+  SaturdayLineup.updateMany({_id: {$in: checkedArray}}, {$set: {checked: true}}, {multi: true}, (err, checked) => {
+    if (err) console.log(err)
+    else console.log(checked);
+
+    SaturdayLineup.find({checked:true}, (errormessage, newItinerary) => {
+      mongoose.connection.db.dropCollection('saturdayitineraryitems', (er, result) => {
+           SaturdayItinerary.insertMany(newItinerary, (error, docs) => {
+           console.log('done');
+           });
+      });
+    })
+  });
+}
+
+
 
 router.get('/', (req, res) => {
   res.send('itinerary page')
 });
 
 router.post('/', (req, res) => {
-    checkedArray = [];
-    uncheckedArray = [];
-    for (keys in req.body) {
-      if (req.body[keys] == 0) {
-        req.body[keys] = false;
-        uncheckedArray.push(keys)
-      }
-      else {
-        req.body[keys] = true;
-      checkedArray.push(keys);
-    }
-  }
-
-    SaturdayLineup.updateMany({_id: {$in: uncheckedArray}}, {$set: {checked: false}}, {new: true}, (err, data) => {
-      console.log('these weree unchecked:', data);
-    });
-
-    SaturdayLineup.updateMany({_id: {$in: checkedArray}}, {$set: {checked: true}}, {new: true}, (err, data) => {
-      console.log('these weree checked:', data);
-    });
-
-    // console.log('checked array', checkedArray);
-    // console.log('unchecked array', uncheckedArray);
-
-
+    addItineraryItem(req.body)
     res.send(req.body)
 
 })

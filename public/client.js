42e480edf0052ed1055d2d0ddb0f5bca0fa06965
Victor Recabarren <victormrecabarren@gmail.com>
2019-06-03 22:07:02 -0500

    added listeners to all selection checkboxes to detect when input is selected or unselected and sends to hideConflicting and showConflicting functions appropriately

--- public/client.js
+++ public/client.js
@@ -1,3 +1,31 @@
+
+const addListeners = (selection) => {
+  for (let i = 0; i<selection.length; i++) {
+    selection.eq(i).mouseup((event) => {
+      isChecked(event.target);
+    })
+  }
+}
+
+const isChecked = (selected) => {
+  if (!$(selected).prop('checked') == true) {
+    hideConflicting(selected)
+  } else {
+    showConflicting(selected)
+  };
+};
+
+const hideConflicting = (selected) => {
+  console.log('going to hide conflicting acts now, since you selected', selected);
+}
+
+const showConflicting = (unselected) => {
+    console.log('going to display non conflicting acts now, since you unselected', unselected);
+}
+
+
 $(() => {
+  const $selection = $('.selection');
+  addListeners($selection)
 
 })
